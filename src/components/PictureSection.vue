a59b7c52b19977de773b148012ffcbd4cd31ca0c
hyunchang Park <hyunchang1234@hanmail.net>
2023-09-27 16:26:04 +0900

    1차 완성본

---
+++ src/components/PictureSection.vue
@@ -0,0 +1,177 @@
+<script setup>
+    import { pictureText } from "@/constants/index";
+</script>
+
+<template>
+    <section id="picture">
+        <div class="picture__inner">
+            <div class="picture__title">
+                picture <em>사진</em>
+            </div>
+            <div class="picture__wrap">
+                <div class="picture__item" v-for="(picture, key) in pictureText" :key="key">
+                    <span class="num">{{ picture.num }}.</span>
+                    <img :src="picture.img" :alt="picture.name" />
+                    <h3 class="title">{{ picture.title }}</h3>
+                    <!-- <p class="desc">{{ picture.desc }}</p> -->
+                    <!-- <a :href="picture.view" target="_blank" class="site">사이트 보기</a> -->
+                </div>
+            </div>
+        </div>
+    </section>
+</template> 
+
+<script>
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default {
+    mounted: function(){
+        this.scrollAnimation();
+    },
+    methods: {
+        scrollAnimation(){
+            const horSection = gsap.utils.toArray(".picture__item");
+
+            gsap.to(horSection, {
+                xPercent: -120 * (horSection.length - 1),
+                ease: "none",
+                scrollTrigger: {
+                    trigger: "#picture",
+                    start: "top 56px",
+                    end: "+=3000",
+                    pin: true,
+                    scrub: 1,
+                    markers: false,
+                    invalidateOnRefresh: true,
+                    anticipatePin: 1,
+                },
+            });
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#picture {
+    width: 100%;
+    margin-top: 30vh;
+    // overflow: hidden;
+}
+.picture__inner {
+    padding: 16px;
+
+    .picture__title {
+        width: 100%;
+        height: 5vw;
+        font-size: 4vw;
+        font-weight: 900;
+        line-height: 1.6;
+        font-family: var(--mainKor-font);
+        text-transform: uppercase;
+        color: var(--black100);
+        border-bottom: 0.4vw solid var(--black100);
+        margin-bottom: 16px;
+        text-indent: -0.26vw;
+
+        em {
+            font-size: 1.25rem;
+            font-weight: 400;
+            line-height: 2;
+        }
+    }
+    .picture__wrap {
+        display: flex;
+        flex-wrap: wrap;
+        width: 7000px;
+
+        .picture__item {
+            width: 500px;
+            height: 100vh;
+            background-color: var(--subBg100);
+            padding: 2.5rem;
+            margin-right: 20px;
+
+            &.p1 {
+                background-color: var(--subBg100);
+            }
+            &.p2 {
+                background-color: var(--subBg200);
+            }
+            &.p3 {
+                background-color: var(--subBg300);
+            }
+            &.p4 {
+                background-color: var(--subBg400);
+            }
+            &.p5 {
+                background-color: var(--subBg500);
+            }
+            &.p6 {
+                background-color: var(--subBg100);
+            }
+            &.p7 {
+                background-color: var(--subBg200);
+            }
+            &.p8 {
+                background-color: var(--subBg300);
+            }
+            &.p9 {
+                background-color: var(--subBg400);
+            }
+            &.p10 {
+                background-color: var(--subBg500);
+            }
+            .num {
+                font-size: 2rem;
+                font-family: var(--mainNum-font);
+            }
+            .img {
+                display: block;
+                margin-top: -20px;
+                transition: margin-top 0.3s;
+
+                img {
+                    border-radius: 5px;
+                    filter: saturate(0%);
+                    transition: all 0.3s;
+                }
+                
+                &:hover {
+                    margin-top: 0;
+
+                    img {
+                        filter: saturate(100%);
+                    }
+                }
+            }
+            .title {
+                font-size: 1.5rem;
+                text-align: center;
+                padding: 0.8rem 0;
+                font-weight: 700;
+                color: var(--black100);
+                border-bottom: 2px solid var(--black100);
+                margin-bottom: 1rem;
+            }
+            .desc {
+                font-size: 1rem;
+            }
+            .site {
+                border: 1px solid var(--black100);
+                display: block;
+                text-align: center;
+                padding: 0.625rem 1.5rem;
+                margin-top: 1.5rem;
+                transition: all 0.2s;
+
+                &:hover {
+                    background-color: var(--black100);
+                    color: var(--white);
+                }
+            }
+        }
+    }
+}
+</style>
---
+++ src/constants/index.js
@@ -0,0 +1,174 @@
+export const headerNav = [
+    {
+        title: "intro",
+        url: "#intro",
+    },
+    {
+        title: "skill",
+        url: "#skill",
+    },
+    {
+        title: "site",
+        url: "#site",
+    },
+    {
+        title: "portfolio",
+        url: "#port",
+    },
+    {
+        title: "contact",
+        url: "#contact",
+    },
+];
+
+export const introText = {
+    // title: "수혀니 ♡ 현창이",
+    title: "100",
+    desc: ["20230917", "100일", "♡"],
+};
+
+export const skillText = [
+    {
+        title: "20230604",
+        desc: "첫만남",
+    },
+    {
+        title: "20230917",
+        desc: "since 20230610 ~ 20230917   100일",
+    },
+    {
+        title: "오래오래",
+        desc: "많이많이",
+    },
+];
+
+export const siteText = [
+    {
+        text: ["make", "site compliant with", "webstandard"],
+        title: "비트를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-vite",
+        view: "https://port2023-vite.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, Vite",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-react",
+        view: "https://port2023-react.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, Vite",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "vue.js"],
+        title: "뷰를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-vue",
+        view: "https://port2023-vue.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, Scss Variable, vue",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "next.js"],
+        title: "넥스트를 이용한 사이트 제작",
+        code: "https://github.com/webstoryboy/port2023-next",
+        view: "https://port2023-next.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, Scss Variable, next.js",
+        ],
+    },
+];
+
+
+import picture01 from "../assets/img/100-1.jpg";
+import picture02 from "../assets/img/100-2.jpg";
+import picture03 from "../assets/img/100-3.jpg";
+import picture04 from "../assets/img/100-4.jpg";
+import picture05 from "../assets/img/100-5.jpg";
+import picture06 from "../assets/img/100-6.jpg";
+import picture07 from "../assets/img/100-7.jpg";
+import picture08 from "../assets/img/100-8.jpg";
+import picture09 from "../assets/img/100-9.jpg";
+import picture10 from "../assets/img/100-10.jpg";
+
+export const pictureText = [
+    {
+        num: "01",
+        //title: "어워드에도 올라간 포트폴리오",
+        //desc: "라마 디자인을 통해 자신의 스킬을 가장 멋지게 표현한 포트폴리오입니다. 가로 모드와 세로 모드는 매우 인상적이며 특히 리액트와 비트를 이용하여 제작한 것은 더욱 놀랍습니다. 이 사이트는 awwwards.com에도 인정받아 올라간 포트폴리오입니다. 확실히 그의 뛰어난 디자인 스킬과 기술력이 빛을 발휘한 결과물인 것 같습니다.",
+        img: picture01,
+        //code: "https://github.com/kimsangjunv1/-React-Portfolio",
+        //view: "https://portfoliosj-react.netlify.app",
+        //name: "김상* 포트폴리오",
+    },
+    {
+        num: "02",
+        img: picture02,
+    },
+    {
+        num: "03",
+        img: picture03,
+    },
+    {
+        num: "04",
+        img: picture04,
+    },
+    {
+        num: "05",
+        img: picture05,
+    },
+    {
+        num: "06",
+        img: picture06,
+    },
+    {
+        num: "07",
+        img: picture07,
+    },
+    {
+        num: "08",
+        img: picture08,
+    },
+    {
+        num: "09",
+        img: picture09,
+    },
+    {
+        num: "10",
+        img: picture10,
+    },
+];
+
+export const contactText = [
+    {
+        link: "https://open.kakao.com/",
+        title: "KAKAO : hyunchang",
+    },
+    {
+        link: "mailto:hyunchang1234@hanmail.net",
+        title: "mail : hyunchang1234@hanmail.net",
+    },
+];
+
+export const footerText = [
+    {
+        title: "github",
+        desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
+        link: "https://github.com/ParkHyunchang",
+    },
+    {
+        title: "blog",
+        desc: "블러그에 오시면 더 많은 정보를 볼 수 있습니다.",
+        link: "https://hyunchang88.tistory.com/",
+    }
+];
